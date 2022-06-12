cd5774ec51a061dca2ef07fd1a5af7af79567f94
christine <cfayez>
2022-06-12 20:10:37 +0200

    edit2

--- library/models/library_book.py
+++ library/models/library_book.py
@@ -124,6 +124,8 @@ class LibraryBook(models.Model):
 
         for book in self.filtered('date_release'):
             d = today - timedelta(days=book.age_days)
+            print(d)
+
         book.date_release = d
 
     @api.constrains('date_release')
